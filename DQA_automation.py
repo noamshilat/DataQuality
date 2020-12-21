42dd2dd43a0b791446e3fb1cca83180ab6010c91
noamshilat <76401311+noamshilat@users.noreply.github.com>
2020-12-21 19:50:50 +0200

    Update DQA_automation.py

--- DQA_automation.py
+++ DQA_automation.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import re
 from datetime import *
-
 ############################################################
 # DataFrame and Dictionaries
 # Get the data
