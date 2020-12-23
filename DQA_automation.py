5b3cdc6afd44c43ebfc074417e87aa80ef6df301
noamshilat <76401311+noamshilat@users.noreply.github.com>
2020-12-23 12:54:12 +0200

    Update DQA_automation.py

--- DQA_automation.py
+++ DQA_automation.py
@@ -1,171 +1,128 @@
-import pandas as pd
-import numpy as np
-import re
-from datetime import *
-############################################################
-# DataFrame and Dictionaries
-# Get the data
-df = pd.read_csv('')
-# if needed, get additional data like dictionaries and there list of values.
-dict_column = pd.read_csv('')['']
-###########################################################
-def data_setup():
-    answer = input('Have you setup a DataFrame and Dictionaries for work?(Y/N) ')
-    if answer != 'Y':
-      raise Exception("Please setup DataFrame and Dictionaries. Then, try again..")
-###########################################################
-def preliminary_settings():
-    global table_name, table_list,column_list, row_index_list, original_value_list, dq_test_type_list, runtime_list
-    table_name = input('What is your table name? ')
-    table_list = []
-    column_list = []
-    row_index_list = []
-    original_value_list = []
-    dq_test_type_list = []
-    runtime_list = []
-############################################################
-def null_test(df):
-    for column in df.columns:
-        for index, value in enumerate(df[column]):
-            if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))== 0) or (len(re.sub('^\s*$','',str(value)))== 0) or (pd.isnull(value)):
-                table_list.append(table_name),
-                column_list.append(column),
-                original_value_list.append(value),
-                row_index_list.append(index),
-                dq_test_type_list.append('null_test'),
-                runtime_list.append(str(datetime.now()))
-################################################################
-def alphanumeric_test(df):
-    column2check = input(f'''Please select columns to check for alphanumeric only: {(', '.join(df.columns))}.
-your choices are(column1,column2)... ''').split(',')
+class DataQuality:
     
-    for column in df[column2check]:
-        for index, value in enumerate(df[column]):
-            if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
-                    if len(re.sub('^[a-zA-Z0-9]*$','',str(value)))> 0:
-                        table_list.append(table_name),
-                        column_list.append(column),
-                        original_value_list.append(value),
-                        row_index_list.append(index),
-                        dq_test_type_list.append('alphanumeric_test'),
-                        runtime_list.append(str(datetime.now()))
-################################################################
-def name_test(df):
-    column2check = input(f'''Please select columns to check for names(A-z and א-ת) only: {(', '.join(df.columns))}.
-your choices are(column1,column2)... ''').split(',')
+    def __init__(self,table_name,df):
+        self.table_name = table_name
+        self.table_list = []
+        self.column_list = []
+        self.row_index_list = []
+        self.original_value_list = []
+        self.dq_test_type_list = []
+        self.runtime_list = []
+        self.df = df
     
-    for column in df[column2check]:
-        for index, value in enumerate(df[column]):
-            if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
-                    if len(re.sub('^[a-zA-Zא-ת\s]*$','',str(value)))> 0:
-                        table_list.append(table_name),
-                        column_list.append(column),
-                        original_value_list.append(value),
-                        row_index_list.append(index),
-                        dq_test_type_list.append('name_test'),
-                        runtime_list.append(str(datetime.now()))
-################################################################
-def digit_test(df):
-    column2check = input(f'''Please select columns to check for digit only: {(', '.join(df.columns))}.
-your choices are(column1,column2)... ''').split(',')
+    def null_test(self):
+        for column in self.df.columns:
+            for index, value in enumerate(self.df[column]):
+                if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))== 0) or (len(re.sub('^\s*$','',str(value)))== 0) or (pd.isnull(value)):
+                    self.table_list.append(self.table_name),
+                    self.column_list.append(column),
+                    self.original_value_list.append(value),
+                    self.row_index_list.append(index),
+                    self.dq_test_type_list.append('null_test'),
+                    self.runtime_list.append(str(datetime.now()))
     
-    for column in df[column2check]:
-        for index, value in enumerate(df[column]):
-            if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
-                    if len(re.sub('^[0-9]*$','',str(value)))> 0:
-                        table_list.append(table_name),
-                        column_list.append(column),
-                        original_value_list.append(value),
-                        row_index_list.append(index),
-                        dq_test_type_list.append('digit_test'),
-                        runtime_list.append(str(datetime.now()))
-################################################################
-def date_test(df):
-    date_columns = input(f'''Please select date columns: {(', '.join(df.columns))}.
-your choices are(column1,column2)... ''').split(',')
-    for column in df[date_columns]:
-        for index, value in enumerate(df[column]):
-            try:
+    def alphanumeric_test(self,alphanumeric_column2check):
+        for column in self.df[alphanumeric_column2check]:
+            for index, value in enumerate(self.df[column]):
                 if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
-                    if (pd.to_datetime(value) > date.today()) or (pd.to_datetime(value) < pd.to_datetime('1900-01-01')):
-                        table_list.append(table_name),
-                        column_list.append(column),
-                        original_value_list.append(value),
-                        row_index_list.append(index),
-                        dq_test_type_list.append('date_test'),
-                        runtime_list.append(str(datetime.now()))
-            except:
-                table_list.append(table_name),
-                column_list.append(column),
-                original_value_list.append(value),
-                row_index_list.append(index),
-                dq_test_type_list.append('date_test'),
-                runtime_list.append(str(datetime.now()))
-#########################################################
-def phone_test(df):
-    column2check = input(f'''Please select columns to check for israeli phone format: {(', '.join(df.columns))}.
-your choices are(column1,column2)... ''').split(',')
+                        if len(re.sub('^[a-zA-Z0-9]*$','',value))> 0:
+                            self.table_list.append(self.table_name),
+                            self.column_list.append(column),
+                            self.original_value_list.append(value),
+                            self.row_index_list.append(index),
+                            self.dq_test_type_list.append('alphanumeric_test'),
+                            self.runtime_list.append(str(datetime.now()))
     
-    for column in df[column2check]:
-        for index, value in enumerate(df[column]):
-            if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
-                    if len(re.sub('^\+?(972|0)(\-)?0?(([23489]{1}\d{7})|[5]{1}\d{8})$','',str((str(value).replace('-','')).zfill(10))))> 0:
-                        table_list.append(table_name),
-                        column_list.append(column),
-                        original_value_list.append(value),
-                        row_index_list.append(index),
-                        dq_test_type_list.append('phone_test'),
-                        runtime_list.append(str(datetime.now()))
-#########################################################
-def dict_test(df,dict_column):
-    column2check = input(f'''Please select columns to check against list: {(', '.join(df.columns))}.
-your choices are(column1,column2)... ''').split(',')
+    def name_test(self,name_column2check):
+        for column in self.df[name_column2check]:
+            for index, value in enumerate(self.df[column]):
+                if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
+                        if len(re.sub('^[a-zA-Zא-ת\s]*$','',value))> 0:
+                            self.table_list.append(self.table_name),
+                            self.column_list.append(column),
+                            self.original_value_list.append(value),
+                            self.row_index_list.append(index),
+                            self.dq_test_type_list.append('name_test'),
+                            self.runtime_list.append(str(datetime.now()))
     
-    for column in df[column2check]:
-        for index, value in enumerate(df[column]):
-            if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
-                    if str(value) not in [str(item) for item in dict_column]:
-                        table_list.append(table_name),
-                        column_list.append(column),
-                        original_value_list.append(value),
-                        row_index_list.append(index),
-                        dq_test_type_list.append('dict_test'),
-                        runtime_list.append(str(datetime.now()))
-#########################################################
-def duplicate_test(df):
-    column2check = input(f'''Please select columns to set as unique id of row to find duplicate rows: {(', '.join(df.columns))}.
-your choices are(column1,column2)... ''').split(',')
+    def digit_test(self,digit_column2check):
+        for column in self.df[digit_column2check]:
+            for index, value in enumerate(self.df[column]):
+                if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
+                        if len(re.sub('^[0-9]*$','',value))> 0:
+                            self.table_list.append(self.table_name),
+                            self.column_list.append(column),
+                            self.original_value_list.append(value),
+                            self.row_index_list.append(index),
+                            self.dq_test_type_list.append('digit_test'),
+                            self.runtime_list.append(str(datetime.now()))
     
-    for value in df.index[df.duplicated(subset=column2check, keep=False)]:
-        table_list.append(table_name),
-        column_list.append(', '.join([str(item) for item in column2check])),
-        original_value_list.append(', '.join([str(item) for item in df.loc[value,column2check]])),
-        row_index_list.append(value),
-        dq_test_type_list.append('duplicate_test'),
-        runtime_list.append(str(datetime.now()))
-#########################################################
-def error_log_generator():
-    error_log = pd.DataFrame(list(zip(table_list, column_list, row_index_list, original_value_list, 
-                                 dq_test_type_list, runtime_list)), 
-       columns =['Table', 'Column', 'Row_index', 'Original_value', 'DQ_test_type', 'Runtime'])
-    return error_log.drop_duplicates(subset=['Column','Row_index'])
-#########################################################
-data_setup()
-preliminary_settings()
-print()
-null_test(df)
-dict_test(df,dict_column)
-print()
-alphanumeric_test(df)
-print()
-digit_test(df)
-print()
-date_test(df)
-print()
-phone_test(df)
-print()
-name_test(df)
-print()
-duplicate_test(df)
-print()
-error_log_generator()
+    def date_test(self,date_columns):
+        for column in self.df[date_columns]:
+            for index, value in enumerate(self.df[column]):
+                try:
+                    if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
+                        if (pd.to_datetime(value) > date.today()) or (pd.to_datetime(value) < pd.to_datetime('1900-01-01')):
+                            self.table_list.append(self.table_name),
+                            self.column_list.append(column),
+                            self.original_value_list.append(value),
+                            self.row_index_list.append(index),
+                            self.dq_test_type_list.append('date_test'),
+                            self.runtime_list.append(str(datetime.now()))
+                except:
+                    self.table_list.append(self.table_name),
+                    self.column_list.append(column),
+                    self.original_value_list.append(value),
+                    self.row_index_list.append(index),
+                    self.dq_test_type_list.append('date_test'),
+                    self.runtime_list.append(str(datetime.now()))
+                    
+    def phone_test(self,phone_column2check):
+        for column in self.df[phone_column2check]:
+            for index, value in enumerate(self.df[column]):
+                if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
+                        if len(re.sub('^\+?(972|0)(\-)?0?(([23489]{1}\d{7})|[5]{1}\d{8})$','',value.replace('-','').zfill(10)))> 0:
+                            self.table_list.append(self.table_name),
+                            self.column_list.append(column),
+                            self.original_value_list.append(value),
+                            self.row_index_list.append(index),
+                            self.dq_test_type_list.append('phone_test'),
+                            self.runtime_list.append(str(datetime.now()))
+                            
+    def dict_test(self,dict_column2check,list_column):
+        for column in self.df[dict_column2check]:
+            for index, value in enumerate(self.df[column]):
+                if (len(re.sub('[!@#$%^&*(),.?":{}|<>]','',str(value)))> 0) and (len(re.sub('^\s*$','',str(value)))> 0) and (pd.isnull(value) is False):
+                        if value not in [str(item) for item in list_column]:
+                            self.table_list.append(self.table_name),
+                            self.column_list.append(column),
+                            self.original_value_list.append(value),
+                            self.row_index_list.append(index),
+                            self.dq_test_type_list.append('dict_test'),
+                            self.runtime_list.append(str(datetime.now()))
+                            
+    def context_test(self,context_column2check,context_column2check_value,context_column2check_against,context_list2check):
+        for index, value in enumerate(self.df[context_column2check]):
+            if self.df.loc[index, ''.join(context_column2check)] == str(context_column2check_value):
+                if self.df.loc[index,''.join(context_column2check_against)] not in [str(item) for item in context_list2check]:
+                    self.table_list.append(self.table_name),
+                    self.column_list.append(''.join(context_column2check_against)),
+                    self.original_value_list.append(self.df.loc[index,''.join(context_column2check_against)]),
+                    self.row_index_list.append(index),
+                    self.dq_test_type_list.append('context_test'),
+                    self.runtime_list.append(str(datetime.now()))
+                    
+    def duplicate_test(self,duplicate_column2check):
+        for value in self.df.index[self.df.duplicated(subset=duplicate_column2check, keep=False)]:
+            self.table_list.append(self.table_name),
+            self.column_list.append(', '.join([str(item) for item in duplicate_column2check])),
+            self.original_value_list.append(', '.join([item for item in self.df.loc[value,duplicate_column2check]])),
+            self.row_index_list.append(value),
+            self.dq_test_type_list.append('duplicate_test'),
+            self.runtime_list.append(str(datetime.now()))
+            
+    def error_log_generator(self):
+        error_log = pd.DataFrame(list(zip(self.table_list, self.column_list, self.row_index_list, self.original_value_list, 
+                                     self.dq_test_type_list, self.runtime_list)), 
+           columns =['Table', 'Column', 'Row_index', 'Original_value', 'DQ_test_type', 'Runtime'])
+        return error_log.drop_duplicates(subset=['Column','Row_index']).reset_index()
